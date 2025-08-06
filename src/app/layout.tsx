3d24792a0746545412833f358e7ac30788f0918d
IsaacVNX <isaacmoura.c69@gmail.com>
2025-08-06 16:25:39 -0300

    Ajuste

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,9 +6,6 @@ import { Toaster } from "@/components/ui/toaster"
 export const metadata: Metadata = {
   title: 'Gestão de Obras',
   description: 'Sistema de Gestão de Obras do Grupo Matos',
-  icons: {
-    icon: 'https://grupomatos.ind.br/storage/2024/06/Perfil-Grupo-Matos2.png',
-  },
 };
 
 export default function RootLayout({
@@ -19,6 +16,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        <link rel="icon" href="https://grupomatos.ind.br/storage/2024/06/Perfil-Grupo-Matos2.png" type="image/png" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
